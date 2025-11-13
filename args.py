4252fa8a4540695b0058ddda49cf72d7108cdafa
Tatiana Tsvetkova <137390908+tsvetkovatatiana@users.noreply.github.com>
2025-11-13 22:36:11 +0200

    add new args: -weight decay, -scheduler and others

--- args.py
+++ args.py
@@ -1,9 +1,10 @@
 import argparse
 
+
 def get_args():
     parser = argparse.ArgumentParser(description="Model training options")
 
-    parser.add_argument("-backbone", type=str, default="resnet18",
+    parser.add_argument("-backbone", type=str, default="resnet34",
                         choices=["resnet18", "resnet34", "resnet50"])
 
     parser.add_argument("-csv_dir", type=str, default="data/CSVs")
@@ -15,7 +16,19 @@ def get_args():
 
     parser.add_argument("-lr", type=float, default=1e-3)
 
-    parser.add_argument("-epochs", type=float, default=5)
+    parser.add_argument("-epochs", type=float, default=20)
+
+    parser.add_argument('--weight_decay', type=float, default=1e-4)
+
+    parser.add_argument("-scheduler", type=str, default="none",
+                        choices=["cosine", "plateau", "step", "none"],
+                        help="Learning rate scheduler type")
+    parser.add_argument("-min_lr", type=float, default=1e-3,
+                        help="Minimum LR for cosine scheduler")
+    parser.add_argument("-step_size", type=int, default=10,
+                        help="Step size for StepLR (if used)")
+    parser.add_argument("-gamma", type=float, default=0.5,
+                        help="Gamma decay factor for StepLR/Plateau")
 
     args = parser.parse_args()
 
